52fbf7c361c36723ebb233c117e82243f8f7c4ff
jungYoungSick <junfas1216@gmail.comgit config --global user.email junfas1216@gmail.com>
2023-10-12 10:43:05 +0900

    서버 작성 완료

---
+++ app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const server = http.createServer((req,res) => {
+
+});
+
+server.listen(3500, (err)=> {
+  if(!err) {
+    console.error("서버 미작동")
+  } else{
+    console.log("3500 포트에 돌아감 http://localhost:3500");
+  }
+});
